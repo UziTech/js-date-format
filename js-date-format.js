a91819691cfb69237a0265c8d4555faea8a8a4ed
UziTech <tbrix13+github.com@gmail.com>
2014-05-11 01:13:25 -0500

    Update js-date-format.js

--- js-date-format.js
+++ js-date-format.js
@@ -1,40 +1,168 @@
 (function(){
-'use strict'
+"use strict"
+
+  Date.prototype.setLocale = function(lang) {
+    if(lang && lang in Date.locales){
+      this.locale = lang;
+    }
+  };
+
+  Date.prototype.getLocale = function() {
+    return this.locale;
+  };
+  
+  Date.prototype.getMonthName = function() {
+    this.locale = this.locale || "en";
+    return Date.locales[this.locale].month_names[this.getMonth()];
+  };
+
+  Date.prototype.getMonthNameShort = function() {
+    this.locale = this.locale || "en";
+    return Date.locales[this.locale].month_names_short[this.getMonth()];
+  };
+
+  Date.prototype.getDayName = function() {
+    this.locale = this.locale || "en";
+    return Date.locales[this.locale].day_names[this.getDay()];
+  };
+
+  Date.prototype.getDayNameShort = function() {
+    this.locale = this.locale || "en";
+    return Date.locales[this.locale].day_names_short[this.getDay()];
+  };
+  
+  Date.locales = {
+    en: {
+      month_names: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+      month_names_short: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+      day_names: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+      day_names_short: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      day_suffix: function(day){
+        var day10 = Math.floor(day / 10);
+        var day1 = day % 10;
+        if(day10 == 1 || day1 > 3 || day1 == 0) {
+          return "th";
+        } else if(day1 == 1) {
+          return "st";
+        } else if(day1 == 2) {
+          return "nd";
+        } else if(day1 == 3) {
+          return "rd";
+        } else {
+          return false;
+        }
+      }
+    }
+  };
+  
   Date.prototype.format = function(formatString){
-    
-    var addPadding = function(value){
-      return ("0" + value).slice(-2);
+
+    var addPadding = function(value, length){
+      var negative = ((value < 0)? "-" : "");
+      var zeros = "0";
+      for(var i = 2; i < length; i++) {
+        zeros += "0";
+      }
+      return negative + (zeros + Math.abs(value).toString()).slice(-length);
     };
 
 		var year = this.getFullYear();
 		var month = this.getMonth() + 1;
 		var day = this.getDate();
 		var hour = this.getHours();
+    var hour12 = ((hour > 12)? hour - 12 : ((hour < 1)? 12 : hour));
 		var minute = this.getMinutes();
 		var second = this.getSeconds();
 		var millisecond = this.getMilliseconds();
-		var decisecond = millisecond % 100;
-		var centisecond = millisecond % 10;
-		var timezone = this.getTimezoneOffset() / 60;
-
-		return formatString.replace(/YYYY/gi, year)
-  		.replace(/YY/g, year - 2000)
-  		.replace(/MM/g, addPadding(month))
-  		.replace(/M/g, month)
-  		.replace(/DD/g, addPadding(day))
-  		.replace(/D/g, day)
-  		.replace(/HH/g, addPadding(hour))
-  		.replace(/H/g, hour)
-  		.replace(/hh/g, (hour > 12? addPadding(hour - 12) : (hour < 1? 12 : addPadding(hour))))
-  		.replace(/h/g, (hour > 12? hour - 12 : (hour < 1? 12 : hour)))
-  		.replace(/mm/g, addPadding(minute))
-  		.replace(/m/g, minute)
-  		.replace(/ss/g, addPadding(second))
-  		.replace(/s/g, second)
-  		.replace(/fff/g, addPadding(millisecond))
-  		.replace(/ff/g, addPadding(centisecond))
-  		.replace(/f/g, addPadding(decisecond))
-  		.replace(/tt/g, (hour >= 12? "pm" : "am"))
-  		.replace(/TT/g, (hour >= 12? "PM" : "AM"));
+		var decisecond = Math.floor(millisecond / 100);
+		var centisecond = Math.floor(millisecond / 10);
+    var offsetHour = Math.floor(-this.getTimezoneOffset() / 60);
+		var offsetMinutes = -this.getTimezoneOffset() % 60;
+    var monthName = this.getMonthName();
+    var monthNameShort = this.getMonthNameShort();
+    var dayName = this.getDayName();
+    var dayNameShort = this.getDayNameShort();
+    var daySuffix = Date.locales[this.locale].day_suffix(day);
+    
+    var replacements = {
+      YYYY: year,
+  		YY: year - 2000,
+  		MMMM: monthName,
+  		MMM: monthNameShort,
+  		MM: addPadding(month, 2),
+  		M: month,
+  		DDDD: dayName,
+  		DDD: dayNameShort,
+  		DD: addPadding(day, 2),
+  		D: day,
+      S: daySuffix,
+  		HH: addPadding(hour, 2),
+  		H: hour,
+  		hh: addPadding(hour12, 2),
+  		h: hour12,
+  		mm: addPadding(minute, 2),
+  		m: minute,
+  		ss: addPadding(second, 2),
+  		s: second,
+  		fff: addPadding(millisecond, 3),
+  		ff: addPadding(centisecond, 2),
+  		f: decisecond,
+  		zzzz: addPadding(offsetHour, 2) + ":" + addPadding(offsetMinutes, 2),
+  		zzz: offsetHour + ":" + addPadding(offsetMinutes, 2),
+  		zz: addPadding(offsetHour, 2),
+  		z: offsetHour,
+  		tt: (hour >= 12? "pm" : "am"),
+  		TT: (hour >= 12? "PM" : "AM")
+    };
+    
+    
+    var formats = new Array();
+    while(formatString.length > 0) {
+      if(formatString[0] == "\"") {
+        var temp = /"[^"]*"/m.exec(formatString);
+        if(temp == null) {
+          formats.push(formatString.substring(1));
+          formatString = "";
+        } else {
+          temp = temp[0].substring(1, temp[0].length - 1);
+          formats.push(temp);
+          formatString = formatString.substring(temp.length + 2);
+        }
+      } else if(formatString[0] == "'") {
+        var temp = /'[^']*'/m.exec(formatString);
+        if(temp == null) {
+          formats.push(formatString.substring(1));
+          formatString = "";
+        } else {
+          temp = temp[0].substring(1, temp[0].length - 1);
+          formats.push(temp);
+          formatString = formatString.substring(temp.length + 2);
+        }
+      } else if(formatString[0] == "\\") {
+        if(formatString.length > 1) {
+          formats.push(formatString.substring(1, 2));
+          formatString = formatString.substring(2);
+        } else {
+          formats.push("\\");
+          formatString = "";
+        }
+      } else {
+        var foundMatch = false;
+        for(var i = formatString.length; i > 0; i--) {
+          if(formatString.substring(0, i) in replacements) {
+            formats.push(replacements[formatString.substring(0, i)]);
+            formatString = formatString.substring(i);
+            foundMatch = true;
+            break;
+          }
+        }
+        if(!foundMatch) {
+          formats.push(formatString[0]);
+          formatString = formatString.substring(1);
+        }
+      }
+    }
+    
+    return formats.join("");
   };
 })();
