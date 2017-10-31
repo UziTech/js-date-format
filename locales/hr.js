63dea3a305b105dc88561c42695d41af61b1552d
tomhorvat <tomhorvat@gmail.com>
2017-10-31 20:34:06 +0100

    Update croatian translation

--- locales/hr.js
+++ locales/hr.js
@@ -1,7 +1,7 @@
 /* languages from http://momentjs.com */
 Date.locales["hr"] = {
-	month_names: "sječanj_veljača_ožujak_travanj_svibanj_lipanj_srpanj_kolovoz_rujan_listopad_studeni_prosinac".split("_"),
-	month_names_short: "sje._vel._ožu._tra._svi._lip._srp._kol._ruj._lis._stu._pro.".split("_"),
+	month_names: "siječanj_veljača_ožujak_travanj_svibanj_lipanj_srpanj_kolovoz_rujan_listopad_studeni_prosinac".split("_"),
+	month_names_short: "sij._velj._ožu._tra._svi._lip._srp._kol._ruj._lis._stu._pro.".split("_"),
 	day_names: "nedjelja_ponedjeljak_utorak_srijeda_četvrtak_petak_subota".split("_"),
 	day_names_short: "ned._pon._uto._sri._čet._pet._sub.".split("_"),
 	date_suffix: function () {
